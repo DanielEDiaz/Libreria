0ec836c2daef2a5acfbfc78787be4b970923f9d1
1830443 <59951537+1830443@users.noreply.github.com>
2021-03-01 09:57:44 -0600

    Update admin.php

--- bookstore/admin.php
+++ bookstore/admin.php
@@ -1,17 +1,22 @@
 <?php
-	$title = "Administration section";
-	require_once "./template/header.php";
+	 ini_set("default_charset", "UTF-8");
+	 mb_internal_encoding("UTF-8");
+	 ini_set('display_errors', 1);
+	 ini_set('display_startup_errors', 1);
+	 error_reporting(E_ALL);
+	 $title = "Administration section";
+	 require_once "./template/header.php";
 ?>
 
 	<form class="form-horizontal" method="post" action="admin_verify.php">
 		<div class="form-group">
-			<label for="name" class="control-label col-md-4">Name</label>
+			<label for="name" class="control-label col-md-4">Nombre</label>
 			<div class="col-md-4">
 				<input type="text" name="name" class="form-control">
 			</div>
 		</div>
 		<div class="form-group">
-			<label for="pass" class="control-label col-md-4">Pass</label>
+			<label for="pass" class="control-label col-md-4">Contraseña</label>
 			<div class="col-md-4">
 				<input type="password" name="pass" class="form-control">
 			</div>
