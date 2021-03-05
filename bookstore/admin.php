30a9b3db085381f2bee773ef1cf6c83b49c7a5f9
1830443 <59951537+1830443@users.noreply.github.com>
2021-03-05 09:32:35 -0600

    Update admin.php

--- bookstore/admin.php
+++ bookstore/admin.php
@@ -4,7 +4,7 @@
 	 ini_set('display_errors', 1);
 	 ini_set('display_startup_errors', 1);
 	 error_reporting(E_ALL);
-	 $title = "Administration section";
+	 $title = "Administrador";
 	 require_once "./template/header.php";
 ?>
 
