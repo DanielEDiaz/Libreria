e1250bd6372a17794616876b1aafcdfb7525bad6
Daniel Esquivel <58433588+DanielEDiaz@users.noreply.github.com>
2021-03-07 22:45:07 -0600

    Update header.php

--- bookstore/template/header.php
+++ bookstore/template/header.php
@@ -23,20 +23,20 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="index.php">CSE Bookstore</a>
+          <a class="navbar-brand" href="index.php">CheapBooks</a>
         </div>
 
         <!--/.navbar-collapse -->
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
               <!-- link to publiser_list.php -->
-              <li><a href="publisher_list.php"><span class="glyphicon glyphicon-paperclip"></span>&nbsp; Publisher</a></li>
+              <li><a href="publisher_list.php"><span class="glyphicon glyphicon-paperclip"></span>&nbsp; Editoriales</a></li>
               <!-- link to books.php -->
-              <li><a href="books.php"><span class="glyphicon glyphicon-book"></span>&nbsp; Books</a></li>
+              <li><a href="books.php"><span class="glyphicon glyphicon-book"></span>&nbsp; Libors</a></li>
               <!-- link to contacts.php -->
-              <li><a href="contact.php"><span class="glyphicon glyphicon-phone-alt"></span>&nbsp; Contact</a></li>
+              <li><a href="contact.php"><span class="glyphicon glyphicon-phone-alt"></span>&nbsp; Contacto</a></li>
               <!-- link to shopping cart -->
-              <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span>&nbsp; My Cart</a></li>
+              <li><a href="cart.php"><span class="glyphicon glyphicon-shopping-cart"></span>&nbsp; Mi carrito</a></li>
             </ul>
         </div>
       </div>
@@ -47,9 +47,9 @@
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
-        <h1>Welcome to online CSE bookstore</h1>
-        <p class="lead">This site has been made using PHP with MYSQL (procedure functions)!</p>
-        <p>The layout use Bootstrap to make it more responsive. It's just a simple web!</p>
+        <h1>Bienvenido a CheapBooks</h1>
+        <p class="lead">“De los diversos instrumentos inventados por el hombre, el más asombroso es el libro; todos los demás son extensiones de su cuerpo… Sólo el libro es una extensión de la imaginación y la memoria”.</p>
+        <p><strong>Jorge Luis Borges.</strong></p>
       </div>
     </div>
     <?php } ?>
